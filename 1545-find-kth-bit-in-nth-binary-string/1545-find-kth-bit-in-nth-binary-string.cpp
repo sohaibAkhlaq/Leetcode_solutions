83dc252b1eb28867127107deb0fc4b7632021a65
Sohaib Akhlaq | Software Engineer <sohaibikhlaq5@gmail.com>
2026-03-03 20:07:41 +0500

    Time: 0 ms (100%), Space: 7.7 MB (80.15%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    string rev(string s){
-        int i = 0;
-        int j = s.size() - 1;
-        while(i < j){
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++;
-            j--;
+    char solve(int n, int k){
+        if(n == 1) return '0';
+        
+        int len = (1 << n) - 1;
+        int mid = (len / 2) + 1;
+        
+        if(k == mid) return '1';
+        
+        if(k < mid){
+            return solve(n-1, k);
         }
-        return s;
-    }
-
-    string inv(string s){
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '0') s[i] = '1';
-            else s[i] = '0';
+        else{
+            char ch = solve(n-1, len - k + 1);
+            if(ch == '0') return '1';
+            else return '0';
         }
-        return s;
     }
 
     char findKthBit(int n, int k) {
-        string s[n];
-        s[0] = "0";
-        
-        for(int i = 1; i < n; i++){
-            s[i] = s[i-1] + "1" + rev(inv(s[i-1]));
-        }
-        
-        return s[n-1][k-1];
+        return solve(n, k);
     }
 };
