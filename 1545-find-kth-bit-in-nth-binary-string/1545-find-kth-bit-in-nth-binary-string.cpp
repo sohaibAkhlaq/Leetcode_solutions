1c6e610006e79bd672c8935413908ac39886f823
Sohaib Akhlaq | Software Engineer <sohaibikhlaq5@gmail.com>
2026-03-03 20:10:45 +0500

    Time: 0 ms (100%), Space: 7.7 MB (97.44%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
+
+    int lengthOf(int n){
+        if(n == 1) return 1;
+        return 2 * lengthOf(n-1) + 1;
+    }
+
     char solve(int n, int k){
         if(n == 1) return '0';
-        
-        int len = (1 << n) - 1;
-        int mid = (len / 2) + 1;
-        
+
+        int prevLen = lengthOf(n-1);
+        int mid = prevLen + 1;
+
         if(k == mid) return '1';
-        
+
         if(k < mid){
             return solve(n-1, k);
         }
         else{
-            char ch = solve(n-1, len - k + 1);
+            int totalLen = 2 * prevLen + 1;
+            int newK = totalLen - k + 1;   // correct mirror
+            char ch = solve(n-1, newK);
+
             if(ch == '0') return '1';
             else return '0';
         }
