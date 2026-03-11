1e6502bd6313c42e451ffd9b8538f7f5a882d51c
Sohaib Akhlaq | Software Engineer <sohaibikhlaq5@gmail.com>
2026-03-11 19:19:54 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    
-
-    int bitwiseComplement(int n) {
-        if(n==0)return 1;
-        int mask=0;
-        int num=n;
-        while(num){
-           mask<<=1;
-           mask|=1;
-           num>>=1;
+    int bitwiseComplement(int num) {
+        if(num == 0) return 1;
+        if(num == 1) return 0;
+        int ans=0;
+        int cnt=0;
+        while(num>0){
+            if(num%2 == 0 && num/2!=0){
+                ans+=pow(2, cnt);
+            }
+            cnt++;
+            num/=2;
         }
-
-        return mask^n;
+        return ans;
     }
 };
